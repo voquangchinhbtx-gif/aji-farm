fdc50c1e585b400a62b6201adb00e47f8766dd2c
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-12 16:58:42 +0700

    Update aji_farm.py

--- aji_farm.py
+++ aji_farm.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 import json
 import re
 import pandas as pd
@@ -439,6 +440,7 @@ elif menu == "📋 Quy trình & Nhắc nhở":
                     save_data(data)
                     st.success(f"Đã lưu: {p_choice} bón {s_choice}")
                     st.rerun()
+
 
 
 
